dd7e27ee0a6dd559cb0a0c019295eee47d8910a1
Craig Chasseur <spoonboy42@gmail.com>
2017-09-28 22:25:11 -0700

    HTTP range support

--- simplecast.py
+++ simplecast.py
@@ -8,11 +8,14 @@ import http.server
 import mimetypes
 import os
 import os.path
+import re
 import socket
 import threading
 import time
 
 import pychromecast.pychromecast as pychromecast
+
+FILE_COPY_BUFFER_SIZE = 64 * 1024
 
 
 # A global variable containing the path of the single file to be served via
@@ -21,30 +24,115 @@ global_single_file = None
 
 
 class SingleFileHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-  """HTTP request handler that only serves a single file.
+  """HTTP request handler that only serves a single file and supports range.
   """
 
+  def do_HEAD(self):
+    self.range_start, self.range_end = self._GetRange()
+    f = self.send_head()
+    if f:
+      f.close()
+
+  def do_GET(self):
+    self.range_start, self.range_end = self._GetRange()
+    f = self.send_head()
+    if f:
+      try:
+        if self.range_start is None:
+          self.copyfile(f, self.wfile)
+        else:
+          self.copy_range(f, self.wfile)
+      finally:
+        f.close()
+
   def send_head(self):
+    """Sends header common to HEAD and GET requests.
+
+    Returns:
+      file, an open file containing the content to serve.
+    """
     if self.path != "/file":
       self.send_error(404, "File not found")
       return None
-    ctype = self.guess_type(global_single_file)
+
     try:
       f = open(global_single_file, 'rb')
     except OSError:
       self.send_error(404, "File not found")
       return None
+
     try:
-      self.send_response(200)
-      self.send_header("Content-type", ctype)
-      fs = os.fstat(f.fileno())
-      self.send_header("Content-Length", str(fs[6]))
-      self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-      self.end_headers()
-      return f
+      file_stat = os.fstat(f.fileno())
+      if self.range_start is None:
+        self._SendRegularHeaders(file_stat)
+      else:
+        self._SendRangeHeaders(file_stat)
     except:
       f.close()
       raise
+
+    return f
+
+  def copy_range(self, source, outputfile):
+    """Copies the file range from range_start to range_end to output.
+
+    Args:
+      source: file, The open source file to serve.
+      outputfile: file, The open output file (i.e. socket) to write to.
+    """
+    source.seek(self.range_start)
+    remaining = 1 + self.range_end - self.range_start
+    while remaining > 0:
+      read_buffer = source.read(min(FILE_COPY_BUFFER_SIZE, remaining))
+      if not read_buffer:
+        return
+      outputfile.write(read_buffer)
+      remaining -= len(read_buffer)
+
+
+  def _GetRange(self):
+    """Parses the Range header from the request, if any.
+
+    Returns:
+      (Optional[int], Optional[int]), the start and end of the specifed byte
+          range. Either may be None if not specified in the request.
+    """
+    range_header = self.headers["Range"]
+    if not range_header:
+      return (None, None)
+    bytes_regex = re.compile(r"^bytes=(\d+)\-(\d+)?")
+    match = bytes_regex.search(range_header)
+    if not match:
+      return (None, None)
+    if match.group(2) is not None:
+      return (int(match.group(1)), int(match.group(2)))
+    else:
+      return (int(match.group(1)), None)
+
+  def _SendRegularHeaders(self, file_stat):
+    """Sends headers for a regular (non-range) response."""
+    ctype = self.guess_type(global_single_file)
+    self.send_response(200)
+    self.send_header("Content-type", ctype)
+    self.send_header("Content-Length", str(file_stat[6]))
+    self.send_header("Last-Modified", self.date_time_string(file_stat.st_mtime))
+    self.end_headers()
+
+  def _SendRangeHeaders(self, file_stat):
+    """Sends headers for a range response."""
+    ctype = self.guess_type(global_single_file)
+    self.send_response(206)
+    self.send_header("Content-type", ctype)
+    file_size = file_stat[6]
+    if self.range_end is None or self.range_end >= file_size:
+      self.range_end = file_size - 1
+    self.send_header("Content-Range",
+                     "bytes {}-{}/{}".format(self.range_start, self.range_end,
+                                       file_size))
+    self.send_header("Content-Length",
+                     str(1 + self.range_end - self.range_start))
+    self.send_header("Last-Modified", self.date_time_string(file_stat.st_mtime))
+    self.end_headers()
 
 
 class CallableHttpServer(object):
